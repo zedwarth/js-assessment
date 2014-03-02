bb4e06f6853fdb8e8acc017c28402dc472bb4370
Seth Szymanski <zedwarth@gmail.com>
2014-03-02 17:52:38 -0600

    Solve first two tests.

--- app/functions.js
+++ app/functions.js
@@ -3,11 +3,12 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(fn, arr) {
-
+		return fn.apply(this, arr);
     },
 
     speak : function(fn, obj) {
-
+		obj.speak = fn;
+		return obj.speak();
     },
 
     functionFunction : function(str) {
