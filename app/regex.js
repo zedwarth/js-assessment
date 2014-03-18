c294d2e9f27941fe846f2d6a7068c1289363dda8
Seth Szymanski <zedwarth@gmail.com>
2014-03-18 18:35:54 -0500

    Pass the first three regular expression tests.

--- app/regex.js
+++ app/regex.js
@@ -3,23 +3,28 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+		var match = /\d/;
+		return match.test(str);
     },
 
     containsRepeatingLetter : function(str) {
-
+		var match = /([A-Za-z])\1/;
+		return match.test(str);
     },
 
     endsWithVowel : function(str) {
-
+		var match = /[AEIOUaeiou]$/
+		return match.test(str);
     },
 
     captureThreeNumbers : function(str) {
-
+		var match = /a/;
+//		return match.test(str);
     },
 
     matchesPattern : function(str) {
-
+		var match = /a/
+//		return match.test(str);
     },
     isUSD : function(str) {
 
