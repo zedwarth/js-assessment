510230d907095a8aa98ae3dfd562ab66a8bb7ff3
Seth Szymanski <zedwarth@gmail.com>
2014-03-18 16:05:04 -0500

    Pass objects tests.

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+		return fn.call(obj)
     },
 
     alterObjects : function(constructor, greeting) {
-
+		constructor.prototype.greeting = greeting; 
     },
 
     iterate : function(obj) {
+		var list = [];
 
+		for (var i in obj) {
+			if (obj.hasOwnProperty(i)){
+				list.push( i + ": " + obj[i] );
+			};
+		};
+
+		return list;
     }
   };
 });
