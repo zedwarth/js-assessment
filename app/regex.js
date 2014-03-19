a95cac2ca61fb933f7c97a401cceeea9a1db4a67
Seth Szymanski <zedwarth@gmail.com>
2014-03-19 15:58:09 -0500

    Finish regex tests.

--- app/regex.js
+++ app/regex.js
@@ -18,16 +18,22 @@ define(function() {
     },
 
     captureThreeNumbers : function(str) {
-		var match = /a/;
-//		return match.test(str);
+		var match = /(\d{3})/;
+		var result = match.exec(str);
+		if (result) {
+			return result[0];
+		} else {
+			return false;
+		};
     },
 
     matchesPattern : function(str) {
-		var match = /a/
-//		return match.test(str);
+		var match = /^\d{3}-\d{3}-\d{4}$/
+		return match.test(str);
     },
     isUSD : function(str) {
-
+		var match = /^\$\d+(?:,\d{3})*(?:\.[\d]{2})*$/;
+		return match.test(str);
     }
   };
 });
