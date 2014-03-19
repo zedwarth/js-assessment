40de65996c7ce49da456a4dffe0e33d9093f2915
Seth Szymanski <zedwarth@gmail.com>
2014-03-19 17:00:52 -0500

    Solve first two number tests.

--- app/numbers.js
+++ app/numbers.js
@@ -3,19 +3,19 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-
+		return 1 & ( num >> ( bit - 1));
     },
 
     base10: function(str) {
-
+		return parseInt(str, 2);
     },
 
     convertToBinary: function(num) {
-
+		return num.toString(2);
     },
 
     multiply: function(a, b) {
-
+		return a * b;
     }
   };
 });
